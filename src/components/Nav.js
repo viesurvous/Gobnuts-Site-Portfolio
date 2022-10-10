2f0e30c5b1283dcbdcd6f07a76d5561e3a9224eb
levlz <hardy.valpro@gmail.com>
2022-10-10 16:58:32 +0200

    test

---
+++ src/components/Nav.js
@@ -0,0 +1,71 @@
+
+function Nav (){
+    return (
+        <>
+        <div className="nav">
+            <div className="nav-items">
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>  
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div> 
+                 <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+                <div className="nav-item">
+                    <h2>Gobnuts</h2>
+                </div>
+            </div>
+        </div>
+      </>
+
+    )
+}
+
+export default Nav;
