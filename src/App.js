2345d06250417d3b688f0a79b5a389a6bc52f8df
levlz <hardy.valpro@gmail.com>
2022-10-11 18:43:03 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.scss';
-import Nav from './components/Nav';
-import Header from './components/Header';
 
 function App() {
-  return ( 
+  return (
     <div className="App">
-      <Nav/>
-      <Header/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
